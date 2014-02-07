a6b2ab1a42fe356a74ec274e99e5f668e210c27e
tanorion <tanorion@gmail.com>
2014-02-07 15:11:41 +0100

    fixed packages

--- BDD/Class1.cs
+++ BDD/Class1.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace BDD
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void Test1() 
+        {
+            Assert.AreEqual(1, 1);
+        }
     }
 }
